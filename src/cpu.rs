d9bf7f65e8035407b6e874bd18047013c899e50f
Takashiidobe <idobetakashi@gmail.com>
2025-11-26 21:27:01 -0500

    add mincore

--- src/cpu.rs
+++ src/cpu.rs
@@ -4000,7 +4000,7 @@ impl Cpu {
             236 => self.sys_sendfile()?,
 
             // mincore(addr, length, vec)
-            237 => bail!("mincore not yet implemented"),
+            237 => self.sys_mincore()?,
 
             // madvise(addr, length, advice)
             238 => bail!("madvise not yet implemented"),
@@ -6797,6 +6797,37 @@ impl Cpu {
 
         // Just return success - we don't actually change protection bits
         // since the memory is already accessible to the guest
+        Ok(0)
+    }
+
+    /// mincore(addr, length, vec)
+    fn sys_mincore(&mut self) -> Result<i64> {
+        let addr = self.data_regs[1] as usize;
+        let length = self.data_regs[2] as usize;
+        let vec_ptr = self.data_regs[3] as usize;
+
+        const PAGE_SIZE: usize = 4096;
+        let num_pages = (length + PAGE_SIZE - 1) / PAGE_SIZE;
+
+        // Validate the memory range exists
+        if length > 0 {
+            self.memory
+                .guest_to_host(addr, length)
+                .ok_or_else(|| anyhow!("mincore: invalid address range"))?;
+        }
+
+        // Get output vector and mark all pages as resident
+        let vec_host = self
+            .memory
+            .guest_to_host_mut(vec_ptr, num_pages)
+            .ok_or_else(|| anyhow!("mincore: invalid vec buffer"))?;
+
+        // Mark all pages as resident (bit 0 = 1)
+        // All valid guest memory is resident in the emulator
+        unsafe {
+            std::ptr::write_bytes(vec_host, 1, num_pages);
+        }
+
         Ok(0)
     }
 
